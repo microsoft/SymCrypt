b47d2be06ac59a84f47795645acc7118fde3f026
Samuel Lee <saml@microsoft.com>
2020-02-04 13:38:38 +0000

    SymCrypt: unittest avoid clamping in arg parsing
    + When wanting to try to repro an issue which may have depended on
    rngseed found that use of `strtol` was causing undesired clamping to
    0x7fffffff
      + Replace strtol with `strtoul` where appropriate

--- unittest/lib/main.cpp
+++ unittest/lib/main.cpp
@@ -699,7 +699,7 @@ processSingleOption( _In_ PSTR option )
             char * endptr;
             __analysis_assume( strlen(option) >= 8 );
             g_profile = TRUE;
-            g_profile_iterations = (UINT32) strtol( &option[8], &endptr, 0 );
+            g_profile_iterations = (UINT32) strtoul( &option[8], &endptr, 0 );
             if (g_profile_iterations == 0)
             {
                 FATAL( "Number of profile iterations must be greater than zero." );
@@ -715,7 +715,7 @@ processSingleOption( _In_ PSTR option )
             {
                 FATAL2( "Key parameter present without profile parameter \"%s\"", &option[0] );
             }
-            g_profile_key = (UINT32) strtol( &option[4], &endptr, 16 );
+            g_profile_key = (UINT32) strtoul( &option[4], &endptr, 16 );
             optionHandled = TRUE;
         }
 
@@ -736,7 +736,7 @@ processSingleOption( _In_ PSTR option )
             char * endptr;
             __analysis_assume( strlen(option) >= 10 );
             // SAL_readableTo(elementCount(10))
-            g_osVersion = (UINT32) strtol( &option[10], &endptr, 16 );
+            g_osVersion = (UINT32) strtoul( &option[10], &endptr, 16 );
             if( endptr != &option[14] || g_osVersion < 0x0500 || g_osVersion > 0x0800 )
             {
                 FATAL2( "Invalid OS version \"%s\"", &option[10] );
@@ -748,7 +748,7 @@ processSingleOption( _In_ PSTR option )
         {
             char * endptr;
             __analysis_assume( strlen(option) >= 8 );
-            g_rngSeed = (UINT32) strtol( &option[8], &endptr, 16 );
+            g_rngSeed = (UINT32) strtoul( &option[8], &endptr, 16 );
             optionHandled = TRUE;
         }
         if (STRICMP(&option[0], "sgx") == 0)
@@ -1112,7 +1112,7 @@ extern const CHAR * SymCryptBuildString;
 VOID
 initTestInfrastructure( int argc, _In_reads_( argc ) char * argv[] )
 {
-    iprint( "SymCrypt unit test program, " 
+    iprint( "SymCrypt unit test program, "
              "Library version %s\n"
              "Copyright (c) Microsoft Corporation. Licensed under the MIT license.\n", SymCryptBuildString );
 
@@ -1574,7 +1574,7 @@ runPerfTests()
         addRsaKeyGenPerfMsBignum( ptRsaKeygen );
 #endif
         ptRsaKeygen.print( "RSA key generation performance" );
-        printOutput( 0 );                
+        printOutput( 0 );
     }
 
     g_perfTestsRunning = FALSE;
