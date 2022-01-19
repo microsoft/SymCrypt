0c56863b6fe9047409e0f73bc64d882539050b4c
Mitch Lindgren 🦎 <mitchl@microsoft.com>
2022-01-19 15:00:10 +0000

    Merged PR 6846596: Support integrity verification with JUMP_SLOT relocations, including on ARM64
    This change adds support for integrity verification even in the presence of JUMP_SLOT relocations, which can't be removed on ARM64 because `-fno-plt` does not work (at least when cross-compiling with Clang). Rather than trying to actually restore their original values, which normally point into the PLT, we set them to zero in our post-processing script before HMACing the module. These relocation targets are excluded from our FIPS module boundary because they're used for external function calls, which we cannot verify the addresses of at runtime, and which are by definition outside of the FIPS module anyway. A convenient side effect of this is that we no longer require the `-fno-plt` flag on AMD64.
    
    We also exclude `R_X86_64_64`, `R_X86_64_GLOB_DAT` and `R_AARCH64_GLOB_DAT` relocations from the FIPS module. These already have initial values of zero and are also used to refer to external functions.
    
    Tested: symcryptmoduletest_oe, symcryptmoduletest_embedded
    
    Related work items: #36669299

--- module/linux_common/integrity.c
+++ module/linux_common/integrity.c
@@ -8,6 +8,7 @@
 
 #include "precomp.h"
 
+// These placeholder vaulues must match the values in process_fips_module.py
 #define PLACEHOLDER_VALUE 0x8BADF00D
 #define PLACEHOLDER_ARRAY \
 {\
@@ -25,12 +26,29 @@
 // Because these values are modified after compile time, the scalar values must be read using
 // SYMCRYPT_FORCE_READ64 or the compiler may inline the placeholder values, leading to incorrect
 // results at runtime.
+
+// Relative virtual address of the HMAC key. Used to calculate where the module starts in memory
+// at runtime.
 const Elf64_Addr SymCryptVolatileFipsHmacKeyRva = (Elf64_Addr) PLACEHOLDER_VALUE;
+
+// Offset to the end of the FIPS module. Bytes after this offset are not considered part of our
+// FIPS module and are not included in the HMAC digest.
 const Elf64_Off SymCryptVolatileFipsBoundaryOffset = PLACEHOLDER_VALUE;
 
+// Key used for HMAC.
 const unsigned char SymCryptVolatileFipsHmacKey[32] = PLACEHOLDER_ARRAY;
+
+// HMAC digest calculated by our post-processing script after calculation. The HMAC digest
+// we calculate at runtime is compared to this digest.
 unsigned char SymCryptVolatileFipsHmacDigest[SYMCRYPT_HMAC_SHA256_RESULT_SIZE] = PLACEHOLDER_ARRAY;
 
+typedef struct 
+{
+    Elf64_Rela* rela;
+    size_t relaEntryCount;
+    Elf64_Rela* pltRela;
+    size_t pltRelaEntryCount;
+} Elf64_Rela_Info;
 
 void SymCryptModuleUndoRelocation(
     _In_ const Elf64_Addr module_base,
@@ -42,13 +60,23 @@ void SymCryptModuleUndoRelocation(
     switch( ELF64_R_TYPE( rela->r_info ) )
     {
         case R_X86_64_RELATIVE:
+        case R_AARCH64_RELATIVE:
             replacement = *target - (Elf64_Off) module_base;
             break;
         case R_X86_64_64:
         case R_X86_64_GLOB_DAT:
+        case R_X86_64_JUMP_SLOT:
+        case R_AARCH64_GLOB_DAT:
+        case R_AARCH64_JUMP_SLOT:
+            // R_X86_64_64, R_X86_64_GLOB_DAT and R_AARCH64_GLOB_DAT relocations all have initial
+            // values of zero. R_X86_64_JUMP_SLOT and R_AARCH64_JUMP_SLOT relocations have initial
+            // values that point into the PLT, but we set these to zero in our post-processing
+            // script before HMACing the module. These relocation targets are excluded from our
+            // FIPS module boundary because they're used for external function calls, which we
+            // cannot verify the addresses of at runtime, and which are by definition outside of
+            // the FIPS module anyway.
             replacement = 0;
             break;
-        case R_X86_64_JUMP_SLOT:
         default:
             // We cannot handle other relocation types
             SYMCRYPT_FIPS_ASSERT( FALSE );
@@ -60,18 +88,23 @@ void SymCryptModuleUndoRelocation(
 
 void SymCryptModuleFindRelocationInfo(
     _In_ const Elf64_Dyn* const dynStart,
-    _Out_ Elf64_Rela** relaStart,
-    _Out_ size_t* relaEntryCount )
+    _Out_ Elf64_Rela_Info* relaInfo)
 {
+    relaInfo->rela = NULL;
+    relaInfo->relaEntryCount = 0;
+    relaInfo->pltRela = NULL;
+    relaInfo->pltRelaEntryCount = 0;
+
     size_t relaTotalSize = 0;
     size_t relaEntrySize = 0;
+    size_t pltTotalSize = 0;
 
     for( const Elf64_Dyn* dyn = dynStart; dyn->d_tag != DT_NULL; ++dyn )
     {
         switch( dyn->d_tag )
         {
             case DT_RELA:
-                *relaStart = (Elf64_Rela*) dyn->d_un.d_ptr;
+                relaInfo->rela = ( Elf64_Rela* ) dyn->d_un.d_ptr;
                 break;
             
             case DT_RELASZ:
@@ -83,9 +116,17 @@ void SymCryptModuleFindRelocationInfo(
                 break;
             
             case DT_JMPREL:
+                relaInfo->pltRela = ( Elf64_Rela* ) dyn->d_un.d_ptr;
+                break;
+
             case DT_PLTRELSZ:
-                // We cannot handle PLT relocations
-                SYMCRYPT_FIPS_ASSERT( FALSE );
+                pltTotalSize = dyn->d_un.d_val;
+                break;
+
+            case DT_PLTREL:
+                // Make sure PLT entries are DT_RELA entries and not DT_REL; we do not suppport
+                // the latter
+                SYMCRYPT_FIPS_ASSERT( dyn->d_un.d_val == DT_RELA );
                 break;
 
             default:
@@ -93,19 +134,26 @@ void SymCryptModuleFindRelocationInfo(
         }
     }
 
-    SYMCRYPT_FIPS_ASSERT( relaStart != NULL );
+    SYMCRYPT_FIPS_ASSERT( relaInfo->rela != NULL );
     SYMCRYPT_FIPS_ASSERT( relaEntrySize == sizeof( Elf64_Rela ) );
     SYMCRYPT_FIPS_ASSERT( relaTotalSize != 0 && relaTotalSize % relaEntrySize == 0 );
 
-    *relaEntryCount = relaTotalSize / relaEntrySize;
+    relaInfo->relaEntryCount = relaTotalSize / relaEntrySize;
+
+    // On AMD64 there should not be a PLT section, because we can't currently handle AMD64 PLT
+    // relocations
+    if( relaInfo->pltRela != NULL)
+    {
+        SYMCRYPT_FIPS_ASSERT( pltTotalSize != 0 && pltTotalSize % sizeof( Elf64_Rela ) == 0 );
+        relaInfo->pltRelaEntryCount = pltTotalSize / sizeof( Elf64_Rela );
+    }
 }
 
 size_t SymCryptModuleProcessSectionWithRelocations(
     _In_ const Elf64_Addr module_base,
     _In_ const Elf64_Phdr* const programHeader,
     _In_ const Elf64_Dyn* const dynStart,
-    _In_ const Elf64_Rela* const relaStart,
-    _In_ const size_t relaEntryCount,
+    _In_ const Elf64_Rela_Info* const relaInfo,
     _Inout_ SYMCRYPT_HMAC_SHA256_STATE* hmacState )
 {
     // The segment that contains relocations consists of the following sections, in this order:
@@ -147,16 +195,31 @@ size_t SymCryptModuleProcessSectionWithRelocations(
             dyn->d_tag == DT_SYMTAB ||
             dyn->d_tag == DT_RELA ||
             dyn->d_tag == DT_GNU_HASH ||
-            dyn->d_tag == DT_VERSYM )
+            dyn->d_tag == DT_VERSYM ||
+            dyn->d_tag == DT_PLTGOT ||
+            dyn->d_tag == DT_JMPREL)
         {
             dyn->d_un.d_val -= (Elf64_Xword) module_base;
         }
     }
 
     // Now we can process the normal relocations listed in the relocation table
-    for( size_t i = 0; i < relaEntryCount; ++i )
+    for( size_t i = 0; i < relaInfo->relaEntryCount; ++i )
     {
-        const Elf64_Rela* rela = relaStart + i;
+        const Elf64_Rela* rela = relaInfo->rela + i;
+
+        // Find the relocation within the section. Note that for a shared object module,
+        // rela->r_offset is actually a virtual address
+        Elf64_Xword* target = (Elf64_Xword*) ( segmentCopy +
+            (Elf64_Off) rela->r_offset - (Elf64_Off) programHeader->p_vaddr );
+        
+        SymCryptModuleUndoRelocation( module_base, target, rela );
+    }
+
+    // Process the GOT entries
+    for( size_t i = 0; i < relaInfo->pltRelaEntryCount; ++i)
+    {
+        const Elf64_Rela* rela = relaInfo->pltRela + i;
 
         // Find the relocation within the section. Note that for a shared object module,
         // rela->r_offset is actually a virtual address
@@ -176,8 +239,7 @@ size_t SymCryptModuleProcessSectionWithRelocations(
 void SymCryptModuleDoHmac(
     _In_ const Elf64_Addr module_base,
     _In_ const Elf64_Dyn* const dynStart,
-    _In_ const Elf64_Rela* const relaStart, 
-    _In_ const size_t relaEntryCount )
+    _In_ const Elf64_Rela_Info* const relaInfo )
 {
     SYMCRYPT_ERROR scError = SYMCRYPT_NO_ERROR;
     SYMCRYPT_HMAC_SHA256_EXPANDED_KEY hmacKey;
@@ -208,13 +270,13 @@ void SymCryptModuleDoHmac(
             // their entirety without modification. Note that the size in memory of the section may
             // be larger than the size on disk, but again, the additional size in memory is not
             // part of our FIPS boundary
-            SymCryptHmacSha256Append( &hmacState, (const unsigned char*) segmentStart,
+            SymCryptHmacSha256Append( &hmacState, (PCBYTE) segmentStart,
                 programHeader->p_filesz );
         }
         else
         {
-            SymCryptModuleProcessSectionWithRelocations( module_base, programHeader, dynStart, relaStart,
-                relaEntryCount, &hmacState );
+            SymCryptModuleProcessSectionWithRelocations( module_base, programHeader, dynStart,
+                relaInfo, &hmacState );
         }
     }
 
@@ -238,15 +300,14 @@ void SymCryptModuleVerifyIntegrity()
     const Elf64_Ehdr* header = (Elf64_Ehdr*) module_base;
     SYMCRYPT_FIPS_ASSERT( memcmp(header->e_ident.ident.magic, ElfMagic, sizeof(ElfMagic)) == 0 );
     SYMCRYPT_FIPS_ASSERT( header->e_type == ET_DYN );
-    SYMCRYPT_FIPS_ASSERT( header->e_machine == EM_X86_64 );
+    SYMCRYPT_FIPS_ASSERT( header->e_machine == EM_X86_64 || header->e_machine == EM_AARCH64 );
     SYMCRYPT_FIPS_ASSERT( header->e_version == EV_CURRENT );
     SYMCRYPT_FIPS_ASSERT( header->e_ehsize == sizeof(Elf64_Ehdr) );
     SYMCRYPT_FIPS_ASSERT( header->e_phentsize == sizeof(Elf64_Phdr) );
     
     const Elf64_Phdr* programHeaderStart = (Elf64_Phdr*) ( module_base + header->e_phoff );
 
-    Elf64_Rela* relaStart = NULL;
-    size_t relaEntryCount = 0;
+    Elf64_Rela_Info relaInfo = {};
 
     Elf64_Dyn* dynStart = NULL;
 
@@ -257,12 +318,12 @@ void SymCryptModuleVerifyIntegrity()
         {
             dynStart = (Elf64_Dyn*) (module_base + (Elf64_Off) programHeader->p_vaddr);
 
-            SymCryptModuleFindRelocationInfo( dynStart, &relaStart, &relaEntryCount );
+            SymCryptModuleFindRelocationInfo( dynStart, &relaInfo );
 
             // We only expect one PT_DYNAMIC segment
             break;
         }
     }
 
-    SymCryptModuleDoHmac( module_base, dynStart, relaStart, relaEntryCount );
+    SymCryptModuleDoHmac( module_base, dynStart, &relaInfo );
 }
--- scripts/process_fips_module.py
+++ scripts/process_fips_module.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 This script facilitaties module integrity verification for FIPS 140 by processing the input ELF
-shared object module and replacing key variables so that the module can calculate its own base
+shared object module and replacing key ElfFileValueProxys so that the module can calculate its own base
 address, and thereby HMAC its own memory, at runtime.
 
 Requires PyElfTools: https://github.com/eliben/pyelftools
@@ -18,62 +18,120 @@ import os
 import secrets
 import stat
 import struct
-import sys
 
+from elftools.elf.constants import P_FLAGS
 from elftools.elf.elffile import ELFFile
-from elftools.elf.sections import Section, SymbolTableSection, Symbol
-from elftools.elf.segments import Segment
+from elftools.elf.enums import ENUM_RELOC_TYPE_x64, ENUM_RELOC_TYPE_AARCH64
 
+# Names of global constants in the FIPS module that need to be replaced
 KEY_NAME = "SymCryptVolatileFipsHmacKey"
 KEY_RVA_NAME = "SymCryptVolatileFipsHmacKeyRva"
 BOUNDARY_OFFSET_NAME = "SymCryptVolatileFipsBoundaryOffset"
 DIGEST_NAME = "SymCryptVolatileFipsHmacDigest"
 
-PLACEHOLDER_VALUE = struct.pack("Q", 0x8BADF00D)
+# Friendly names for Python struct functionality
+CHAR_FORMAT_SPECIFIER = "s"
+QWORD_FORMAT_SPECIFIER = "Q"
+QWORD_BYTE_SIZE = struct.calcsize(QWORD_FORMAT_SPECIFIER)
+
+# Must match the placeholder values in integrity.c
+PLACEHOLDER_VALUE = struct.pack(QWORD_FORMAT_SPECIFIER, 0x8BADF00D)
 PLACEHOLDER_ARRAY = bytes((
     0x5B, 0x75, 0xBB, 0xE4, 0x9E, 0x18, 0x03, 0x55,
     0x08, 0x4E, 0x3F, 0xE7, 0x60, 0x7E, 0x4F, 0x08,
     0xAA, 0x77, 0x0F, 0x0B, 0xAB, 0xC6, 0x58, 0x5A,
     0xA9, 0x9F, 0x83, 0x4B, 0xD0, 0x6E, 0x67, 0x05))
 
-# Writeable flag for segments since elftools doesn't define it
-PF_W = 2
-
-class Variable(object):
+class ElfFileValueProxy(object):
     """
-    Wrapper for a pyelftools Symbol which makes it easier to get the value and offset/
-    virtual address of the object that the symbol represents.
+    Wrapper for a data of arbitrary location, size and type in an ELF file. Unlike pyelftools
+    native types, this wrapper class provides an easy way to write updated values back to the
+    stream. The data may or may not have an associated symbol. Does not support values within
+    compressed sections.
+
+    The following members will always be set:
+        elf_file: The pyelftools ELFFile object that the value is part of
+        section: The pyselftools Section object that the value is part of
+        offset: The offset in the ELF file where the value exists
+        length: The length of the value in bytes
+
+    The following members will only be set for value proxies with corresponding symbols.
+    For value proxies which do not have symbols (e.g. relocation targets), they will be
+    None.
+        symbol: The pyelftools Symbol object corresponding to the value
+        name: The name of the symbol
     """
 
-    def __init__(self, elf_file, name):
+    def __init__(self, elf_file, section, offset, vaddr, length, symbol = None, name = None):
         """
-        Initializes a Variable object by finding the symbol name in the symbol table and mapping
+        Default initializer
+        """
+
+        assert(bool(section.compressed) is False)
+
+        self.elf_file = elf_file
+        self.section = section
+        self.offset = offset
+        self.vaddr = vaddr
+        self.length = length
+        self.symbol = symbol
+        self.name = name
+
+
+    @classmethod
+    def from_vaddr(self, elf_file, vaddr, length):
+        """
+        Creates an ElfFileValueProxy object from a caller-provided virtual address and length.
+        This is useful for creating value proxies for from relocation entries which do not have
+        an associated symbol.
+        """
+
+        section = None
+        for cur_section in elf_file.iter_sections():
+            if (cur_section["sh_addr"] < vaddr
+                and cur_section["sh_addr"] + cur_section["sh_size"] >= vaddr + length):
+
+                section = cur_section
+                break
+
+        assert(section is not None)
+
+        section_offset = vaddr - section["sh_addr"]
+        offset = section["sh_offset"] + section_offset
+
+        return ElfFileValueProxy(elf_file, section, offset, vaddr, length)
+
+    @classmethod
+    def from_symbol_name(self, elf_file, name):
+        """
+        Creates an ElfFileValueProxy object by finding the symbol name in the symbol table and mapping
         it to the offset in the file.
         """
 
-        self.elf_file = elf_file
-        self.name = name
-
-        symtab = self.elf_file.get_section_by_name(".symtab")
+        symtab = elf_file.get_section_by_name(".symtab")
         symbols = symtab.get_symbol_by_name(name)
         assert(len(symbols) == 1)
-        self.symbol = symbols[0]
+        symbol = symbols[0]
 
-        self.vaddr = self.symbol.entry["st_value"]
-        self.length = self.symbol.entry["st_size"]
-        self.section = self.elf_file.get_section(self.symbol["st_shndx"])
-        self.section_offset = self.vaddr - self.section.header["sh_addr"]
-        self.offset = self.section.header["sh_offset"] + self.section_offset
+        length = symbol["st_size"]
+        section = elf_file.get_section(symbol["st_shndx"])
+        assert(section is not None)
+
+        vaddr = symbol["st_value"]
+        section_offset = vaddr - section["sh_addr"]
+        offset = section["sh_offset"] + section_offset
+
+        return ElfFileValueProxy(elf_file, section, offset, vaddr, length, symbol, name)
 
     @property
     def value(self):
-        return self.section.data()[self.section_offset:self.section_offset + self.length]
+        self.elf_file.stream.seek(self.offset)
+        return self.elf_file.stream.read(self.length)
 
     @value.setter
     def value(self, value):
         assert(type(value) == bytes)
         assert(len(value) == self.length)
-        assert(bool(self.section.compressed) is False)
 
         # The .data() method returns a bytes object. We can't use it to write back to the original
         # buffer, so we need to find the appropriate section within the stream using sh_offset and
@@ -86,11 +144,12 @@ class Variable(object):
         new_value = struct.pack(format, *args)
         assert(len(new_value) == self.length)
 
-        logging.debug("Changing {} value".format(self.name))
+        if self.name is not None:
+            logging.debug("Changing {} value".format(self.name))
 
         self.value = new_value
 
-def log_variable(var):
+def log_value(var):
     logging.debug("{}: offset {}, virtual address {}, Value {}".format(
         var.name,
         hex(var.offset),
@@ -136,7 +195,7 @@ def hmac_module(loadable_segments, data_section_offset, key, digest, dump_file_p
     logging.debug("Calculated SHA256 digest: {}".format(digest_bytes.hex()))
 
     digest.set_value(str(len(digest_bytes)) + "s", digest_bytes)
-    log_variable(digest)
+    log_value(digest)
 
 def process_loadable_segments(elf_file):
     """
@@ -168,7 +227,7 @@ def process_loadable_segments(elf_file):
                 logging.error("Found more than one loadable, writeable segment!")
                 raise RuntimeError
 
-            if segment["p_flags"] & PF_W != 0:                
+            if segment["p_flags"] & P_FLAGS.PF_W != 0:                
                 writeable_segment = segment
 
     writeable_segment_sections = []
@@ -186,6 +245,57 @@ def process_loadable_segments(elf_file):
 
     return loadable_segments
 
+def overwrite_jump_slots(elf_file, new_value):
+    """
+    Overwrites the original values of jump slot relocations in the ELF file's stream with the
+    given new_value. Returns a list of the original jump slots as tuples of
+    (vaddr, original value), so that they can be reset after the HMAC digest is calculated.
+    """
+
+    rela_plt_section = elf_file.get_section_by_name(".rela.plt")
+
+    # Jump slot relocations always live in the .rela.plt section. If there is no .rela.plt section,
+    # there will be no jump slot relocations
+    if rela_plt_section is None:
+        return []
+
+    original_jump_slot_values = []
+    for relocation in rela_plt_section.iter_relocations():
+        relocation_type = relocation["r_info_type"]
+        if (relocation_type == ENUM_RELOC_TYPE_x64["R_X86_64_JUMP_SLOT"]
+            or relocation_type == ENUM_RELOC_TYPE_AARCH64["R_AARCH64_JUMP_SLOT"]):
+
+            # Note that r_offset is actually a virtual address
+            relocation_value = ElfFileValueProxy.from_vaddr(elf_file, relocation["r_offset"], QWORD_BYTE_SIZE)
+
+            original_value_int = struct.unpack(QWORD_FORMAT_SPECIFIER, relocation_value.value)[0]
+            original_jump_slot_values.append((relocation_value.vaddr, original_value_int))
+
+            logging.debug("Updating relocation at {} with original value {}".format( 
+                hex(relocation_value.offset), hex(original_value_int)))
+
+            relocation_value.set_value(QWORD_FORMAT_SPECIFIER, new_value)
+        else:
+            logging.warning("Unknown relocation type {} found at offset {}".format(
+                relocation_type, relocation["r_offset"]))
+
+    return original_jump_slot_values
+
+def reset_jump_slots(elf_file, original_jump_slot_values):
+    """
+    Resets the jump slot relocations to their original values, which should be given as a list of
+    tuples of (vaddr, original value). This must be done after HMACing the module to ensure that
+    lazy binding still works.
+    """
+
+    for vaddr, original_value in original_jump_slot_values:
+        relocation_value = ElfFileValueProxy.from_vaddr(elf_file, vaddr, QWORD_BYTE_SIZE)
+
+        logging.debug("Resetting relocation at {} to original value {}".format( 
+                hex(vaddr), hex(original_value)))
+
+        relocation_value.set_value(QWORD_FORMAT_SPECIFIER, original_value)
+
 def main():
     """
     Entrypoint
@@ -193,7 +303,8 @@ def main():
 
     parser = argparse.ArgumentParser(description = "Postprocess SymCrypt shared object module")
     parser.add_argument("input", type=str, help = "Path to SymCrypt module")
-    parser.add_argument("-d", "--debug", action = "store_true", help = "Enable debug output (also dumps hashable module contents to file)")
+    parser.add_argument("-d", "--debug", action = "store_true",
+        help = "Enable debug output (also dumps hashable module contents to file)")
 
     args = parser.parse_args()
 
@@ -206,51 +317,65 @@ def main():
         buffer = input_file.read()
         buffer_stream = io.BytesIO(buffer)
 
-    # Copy the original input file to a backup file
-    os.replace(args.input, args.input + ".bak")
-
     elf_file = ELFFile(buffer_stream)
+
+    arch = elf_file["e_machine"]
+    if not arch == "EM_X86_64" and not arch == "EM_AARCH64":
+        logging.error("Unsupported architecture {}".format(arch))
+        raise RuntimeError
+
+    original_jump_slot_values = overwrite_jump_slots(elf_file, 0)
+
     loadable_segments = process_loadable_segments(elf_file)
 
     # Find the HMAC key placeholder and replace it with a randomly generated key
-    key_variable = Variable(elf_file, KEY_NAME)
+    key_variable = ElfFileValueProxy.from_symbol_name(elf_file, KEY_NAME)
     assert(key_variable.value == PLACEHOLDER_ARRAY)
 
     random_key = secrets.token_bytes(len(key_variable.value)) # NB: not actually a secret
-    key_variable.set_value(str(len(random_key)) + "s", random_key)
-    log_variable(key_variable)
+    key_variable.set_value(str(len(random_key)) + CHAR_FORMAT_SPECIFIER, random_key)
+    log_value(key_variable)
 
     # Find the HMAC key relative virtual address placeholder and replace it with
     # the actual relative virtual address of the HMAC key
-    key_rva_variable = Variable(elf_file, KEY_RVA_NAME)
+    key_rva_variable = ElfFileValueProxy.from_symbol_name(elf_file, KEY_RVA_NAME)
     assert(key_rva_variable.value == PLACEHOLDER_VALUE)
 
-    key_rva_variable.set_value("Q", key_variable.vaddr)
-    log_variable(key_rva_variable)
+    key_rva_variable.set_value(QWORD_FORMAT_SPECIFIER, key_variable.vaddr)
+    log_value(key_rva_variable)
 
     # Find the FIPS module boundary placeholder and replace it with the our actual
     # FIPS module boundary, which we have defined to be the start of the .data section
-    fips_boundary_variable = Variable(elf_file, BOUNDARY_OFFSET_NAME)
+    fips_boundary_variable = ElfFileValueProxy.from_symbol_name(elf_file, BOUNDARY_OFFSET_NAME)
     assert(fips_boundary_variable.value == PLACEHOLDER_VALUE)
 
     data_section = elf_file.get_section_by_name(".data")
     data_section_offset = data_section["sh_offset"]
 
-    fips_boundary_variable.set_value("Q", data_section_offset)
-    log_variable(fips_boundary_variable)
+    fips_boundary_variable.set_value(QWORD_FORMAT_SPECIFIER, data_section_offset)
+    log_value(fips_boundary_variable)
 
     # Find the HMAC digest placeholder, HMAC the loadable segments of the module, and replace
     # the placeholder with the actual digest
-    digest_variable = Variable(elf_file, DIGEST_NAME)
+    digest_variable = ElfFileValueProxy.from_symbol_name(elf_file, DIGEST_NAME)
     assert(digest_variable.value == PLACEHOLDER_ARRAY)
 
     hmac_module(loadable_segments, data_section_offset, key_variable, digest_variable,
         dump_file_path = args.input + ".loadable.bin" if args.debug else None)
 
+    # Reset the jump slot relocation values to their original values so that lazy binding will
+    # still work. We compensate for this at runtime by also overwriting the jump slot values in
+    # our in-memory copy of the module with zeros, so the HMAC digests will still match
+    if len(original_jump_slot_values) > 0:
+        reset_jump_slots(elf_file, original_jump_slot_values)
+
+    # Copy the original input file to a backup file before writing our changes back to the original
+    os.replace(args.input, args.input + ".bak")
+
     with open(args.input, "wb") as output_file:
         output_file.write(buffer_stream.getbuffer())
 
-    # chmod 0755 the new output file so that it"s marked as executable (required by some platforms)
+    # chmod 0755 the new output file so that it's marked as executable (required by some platforms)
     os.chmod(args.input, 
         stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | # User: read, write, execute
         stat.S_IRGRP | stat.S_IXGRP | # Group: read, execute
